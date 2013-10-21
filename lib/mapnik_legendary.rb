16e5e60de361fb2170c470c41ed79d3593c152a9
Andy Allan <andy@gravitystorm.co.uk>
2013-10-21 16:47:35 +0100

    Implement zoom-dependent rendering.

--- lib/mapnik_legendary.rb
+++ lib/mapnik_legendary.rb
@@ -11,6 +11,7 @@ module MapnikLegendary
     map = Mapnik::Map.from_xml(File.read(map_file), false, File.dirname(map_file))
     map.width = legend["width"]
     map.height = legend["height"]
+    map.srs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over"
 
     if legend.has_key?("background")
       map.background = Mapnik::Color.new(legend["background"])
@@ -24,7 +25,7 @@ module MapnikLegendary
     legend["features"].each_with_index do |feature,idx|
 
       # TODO - use a proper csv library rather than .join(",") !
-      geom = Geometry.new(feature["type"]).to_csv
+      geom = Geometry.new(feature["type"], feature["zoom"], map).to_csv
       header = feature["tags"].keys.push("wkt").join(",")
       row = feature["tags"].values.push(geom).join(",")
       datasource = Mapnik::Datasource.create(:type => 'csv', :inline => header + "\n" + row )
@@ -32,7 +33,7 @@ module MapnikLegendary
       map.layers.clear
 
       feature["layers"].each do |layer_name|
-        l = Mapnik::Layer.new(layer_name, "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over")
+        l = Mapnik::Layer.new(layer_name, map.srs)
         l.datasource = datasource
         layer_styles[layer_name].each do |style_name|
           l.styles << style_name
@@ -40,7 +41,7 @@ module MapnikLegendary
         map.layers << l
       end
 
-      map.zoom_to_box(Mapnik::Envelope.new(0,0,1,1))
+      #map.zoom_to_box(Mapnik::Envelope.new(0,0,1,1))
       map.render_to_file("legend#{idx}.png", "png256:t=2")
     end
   end
--- lib/mapnik_legendary/geometry.rb
+++ lib/mapnik_legendary/geometry.rb
@@ -1,11 +1,28 @@
+require 'pry'
+
 module MapnikLegendary
   class Geometry
-    def initialize(type)
+    def initialize(type, zoom, map)
+      zoom = 17 if zoom.nil?
+
+      proj = Mapnik::Projection.new(map.srs)
+      width_of_world_in_pixels = 2 ** zoom * 256
+      width_of_world_in_metres = proj.forward(Mapnik::Coord2d.new(180,0)).x - proj.forward(Mapnik::Coord2d.new(-180,0)).x
+      width_of_image_in_metres = map.width.to_f / width_of_world_in_pixels * width_of_world_in_metres
+      height_of_image_in_metres = map.height.to_f / width_of_world_in_pixels * width_of_world_in_metres
+
+      max_x = width_of_image_in_metres
+      max_y = height_of_image_in_metres
+      min_x = 0
+      min_y = 0
+
       @geom = case type
-              when "point" then "POINT(0.5 0.5)"
-              when "polygon" then "POLYGON((0 0, 1 0, 1 1, 0 1, 0 0))"
-              else "LINESTRING(0 0, 1 1)"
+              when "point" then "POINT(#{max_x/2} #{max_y/2})"
+              when "polygon" then "POLYGON((0 0, #{max_x} 0, #{max_x} #{max_y}, 0 #{max_y}, 0 0))"
+              else "LINESTRING(0 0, #{max_x} #{max_y})"
               end
+
+      map.zoom_to_box(Mapnik::Envelope.new(min_x,min_y,max_x,max_y))
     end
 
     def to_csv
