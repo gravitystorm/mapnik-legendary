4ba43b4386f51fa42057de9d352c5598e8be7202
Andy Allan <andy@gravitystorm.co.uk>
2014-02-04 17:06:58 +0000

    has_key? is apparently deprecated.

--- lib/mapnik_legendary.rb
+++ lib/mapnik_legendary.rb
@@ -17,7 +17,7 @@ module MapnikLegendary
     map.height = legend['height']
     map.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over'
 
-    if legend.has_key?('background')
+    if legend.key?('background')
       map.background = Mapnik::Color.new(legend['background'])
     end
 
