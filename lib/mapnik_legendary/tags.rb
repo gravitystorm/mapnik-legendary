9ef50065553e9a46bee2da808c3dd15d4891651e
Andy Allan <andy@gravitystorm.co.uk>
2014-04-10 16:55:19 +0100

    Handle empty extra tags correctly.

--- lib/mapnik_legendary/tags.rb
+++ lib/mapnik_legendary/tags.rb
@@ -3,6 +3,7 @@
 module MapnikLegendary
   class Tags
     def self.merge_nulls(tags, extras)
+      extras = [] if extras.nil?
       Hash[extras.map { |t| [t, 'null'] }].merge(tags)
     end
   end
